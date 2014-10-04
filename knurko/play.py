5b5a7f97b9c2581955871967e194dba91e05cd1f
Thomas Levine <_@thomaslevine.com>
2014-10-04 23:16:24 +0000

    mplayer

--- knurko/play.py
+++ knurko/play.py
@@ -1,15 +1,7 @@
 import subprocess as sp
 
-bill = sp.Popen(['mplayer', '/home/tlevine/Bill Nye the Science Guy/Water Cycle.avi'], stdout = sp.PIPE, stderr = sp.PIPE)
-stdout, stderr = bill.communicate()
-stdout
-stderr
-stdout
-stdout.split('\n')
-stdout.decode('utf-8')
-stdout.decode('utf-8').split('\n')
-stdout.decode('utf-8').split('\r')
-stdout.decode('utf-8').split('\r')[-2]
-stdout.decode('utf-8').split('\r')[-2]
-history
-
+def play(filename):
+    mplayer = sp.Popen(['mplayer', filename],
+                    stdout = sp.PIPE, stderr = sp.PIPE)
+    stdout, stderr = mplayer.communicate()
+    stdout.decode('utf-8').split('\r')[-2]
