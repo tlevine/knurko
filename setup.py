5fcb566917d9555a6c8e20eda93c2cc738fa3892
Thomas Levine <_@thomaslevine.com>
2014-10-04 23:49:46 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = 'knurko',
+    version = '0.1',
+    description = 'Select a frame from a video file.',
+    author = 'Thomas Levine',
+    author_email = '_@thomaslevine.com',
+    url = 'http://small.dada.pink/knurko',
+    license = 'AGPL',
+    scripts = [os.path.join('bin', 'knurko')],
+    tests_require = [
+        'nose>=1.3.4',
+    ],
+)
